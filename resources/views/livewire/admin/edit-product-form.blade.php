53e760bd85fb6bbaacab5d043818c0dc116afd92
MiguelEstanga <miguelestanga13@gmail.com>
2025-11-23 04:44:49 -0400

    logs2

--- resources/views/livewire/admin/edit-product-form.blade.php
+++ resources/views/livewire/admin/edit-product-form.blade.php
@@ -191,16 +191,7 @@
                 </div>
             @endif
             
-            {{-- Loading state para imagen --}}
-            <div wire:loading wire:target="imagen" class="mt-2">
-                <div class="flex items-center text-sm text-indigo-600">
-                    <svg class="animate-spin -ml-1 mr-2 h-4 w-4 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
-                    Cargando imagen...
-                </div>
-            </div>
+             
         </div>
 
     </form>
@@ -222,16 +213,10 @@
                 wire:loading.attr="disabled"
                 wire:target="updateProduct, imagen"
             >
-                <span wire:loading.remove wire:target="updateProduct, imagen">
+                <span   wire:target="updateProduct, imagen">
                     Actualizar Producto
                 </span>
-                <span wire:loading wire:target="updateProduct, imagen" class="flex items-center">
-                    <svg class="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
-                    Actualizando...
-                </span>
+                
             </button>
         </div>
     </x-slot>
--- resources/views/livewire/admin/edit-product-modal.blade.php
+++ resources/views/livewire/admin/edit-product-modal.blade.php
@@ -1,5 +1,5 @@
 <x-slide-over-modal 
-    title="Editar Producto" 
+    title="Editar Productosss" 
     wire:show="showModal" 
     wireClose="closeModal" 
     maxWidth="lg"
@@ -173,16 +173,6 @@
                 </div>
             @endif
             
-            {{-- Loading state --}}
-            <div wire:loading wire:target="imagen" class="mt-2">
-                <span class="text-sm text-indigo-600">
-                    <svg class="animate-spin -ml-1 mr-2 h-4 w-4 text-indigo-600 inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
-                    Cargando imagen...
-                </span>
-            </div>
         </div>
 
     </form>
@@ -202,7 +192,7 @@
                 form="edit-product-form"
                 class="inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
                 
-                wire:target="updateProduct, imagen"
+                
             >
                 <span  >Actualizar Producto</span>
                 
