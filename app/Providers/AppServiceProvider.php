08fee10e047b83cd51f6fee45eca656ff23f61a6
MiguelEstanga <miguelestanga13@gmail.com>
2025-12-01 01:59:34 -0400

    fix:script

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Maatwebsite\Excel\Facades\Excel;
-use Illuminate\Support\Facades\URL;
-class AppServiceProvider extends ServiceProvider
+ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
@@ -25,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+         
     }
 }
